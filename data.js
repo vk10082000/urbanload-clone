3359400838629112024fbba9190ff54ef7049463
vk10082000 <vamshikrishna99089@gmail.com>
2023-02-25 23:36:31 +0530

    home page completed

--- data.js
+++ data.js
@@ -1,13 +1,580 @@
 [
 {
-    id:,
-    name:,
-    price:,
-    Category:,
-    discriptons:,
-    image:,
-    
+    id:1,
+    name:"Coffee Table In Walnut Morocco Lattice Beige",
+    price:19000,
+    Category:"Coffee-table",
+    discriptons:"Its practical design and large table top make the Kivaha a perfect choice for your living room",
+    image:"https://www.ulcdn.net/images/products/140038/slide/666x363/Kivaha_4_Seater_CF_Table_Set_WLLB_01.jpg?1487065487",
 
+
+},
+{
+    id:2,
+    name:"Blane Square Solid Wood Coffee Table",
+    price:15000,
+    Category:"Coffee-table",
+    discriptons:"Recommended Combos With Blane Square Solid Wood Coffee Table In Teak Finish",
+    image:"https://www.ulcdn.net/images/products/357222/slide/666x363/Blane_Coffee_Table_Set_Jute_Beige_2.jpg?1626926651",
 }
+,
+{
+    id:3,
+    name:"Epsilon Rectangular Solid Wood Coffee Table",
+    price:20000,
+    Category:"Coffee-table",
+    discriptons:"Minimalist geometric style is a signature of the Epsilon family",
+    image:"https://www.ulcdn.net/images/products/218174/slide/666x363/Epsilon_Coffe_Table_TK_01.jpg?1540531356",
+}
+,
+{
+    id:4,
+    name:"Coffee Table In Teak Finish",
+    price:15000,
+    Category:"Coffee-table",
+    discriptons:"Recommended Combos With Blane Square Solid Wood Coffee Table In Teak Finish",
+    image:"https://www.ulcdn.net/images/products/357395/slide/666x363/Friso_Coffee_Table_Set_Velvet_Cream_1.jpg?1626927985",
+}
+,
+{
+    id:5,
+    name:"Walnut Finish Coffee Table",
+    price:12000,
+    Category:"Coffee-table",
+    discriptons:"Recommended Combos With Blane Square Solid Wood Coffee Table In Teak Finish",
+    image:"https://www.ulcdn.net/images/products/119971/slide/666x363/Dyson_Merch.jpg?1583245567",
+}
+,
+{
+    id:6,
+    name:"Coffee Table Set",
+    price:26000,
+    Category:"Coffee-table",
+    discriptons:"Recommended Combos With Blane Square Solid Wood Coffee Table In Teak Finish",
+    image:"https://www.ulcdn.net/images/products/119971/slide/666x363/Dyson_Merch.jpg?1583245567",
+}
+,
+{
+    id:7,
+    name:"Farina Half Leather Sectional Sofa",
+    price:30000,
+    Category:"sofa",
+    discriptons:"Both table and TV unit are available in 2 finishe.Made from solid Mango wood",
+    image:"https://www.ulcdn.net/images/products/313542/product/FNSF52FACA3L_-_category.png?1611552736",
+}
+,
+{
+    id:8,
+    name:"Weston Half Leather Sofa",
+    price:20000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/210346/product/FNSF52WSCL3_-_category.png?1557132449",
+}
+,
+{
+    id:9,
+    name:"Leatherette Sofa Sets Design",
+    price:40000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/183644/original/Lewis_Infinity_Sofa_FNSF51LSCO3SR00SAAAA_slide_00.jpg?1524301923",
+}
+,
+{
+    id:10,
+    name:"Love Seats",
+    price:10000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/283509/slide/666x363/Janet_Lapis_Blue_1.jpg?1566799188",
+}
+,
+{
+    id:11,
+    name:"Minnelli 2 Seater Fabric Loveseat In Indigo",
+    price:12000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/155401/product/Minnelli_Loveseat_00_LP.jpg?1505717720",
+}
+,
+{
+    id:12,
+    name:"Floral 2 Seater Seater Fabric Loveseat In",
+    price:5000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/715761/product/FVSFLC62YL26001_LP.jpg?1671445323",
+},
+{
+    id:13,
+    name:"Nawab 3 Seater Fabric Loveseat",
+    price:50000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/670716/product/FVSTLC51IV24140_LP.jpg?1662008011",
+}
+,
+{
+    id:14,
+    name:"Leather Sofa Sets",
+    price:25000,
+    Category:"sofa",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/taxon_images/taxon/1542/taxon_col_3/Leather-sofa.jpg",
+}
+,
+{
+    id:15,
+    name:"Zephyr Solid Wood Free Standing TV Unit",
+    price:20000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/125612/product/Zephyr_LargeTV_Unit_LP.jpg?1608823390",
+}
+,
+{
+    id:16,
+    name:"Liam Engineered Wood Free Standing TV Unit",
+    price:10000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/579147/product/Liam_Shutter_TV_unit_Classic_WalnutLP_1.jpg?1655449919",
+}
+,
+{
+    id:17,
+    name:"Fenily Engineered Wood Free Standing TV Unit ",
+    price:25000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/565804/product/FVSGAV33WW20528_LP.jpg?1654238681",
+}
+
+,
+{
+    id:18,
+    name:"Iwaki Engineered Wood Swivel TV Unit",
+    price:18999,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/697167/product/LP.jpg?1666975213",
+}
+
+,
+{
+    id:19,
+    name:"Estoye Engineered Wood Wall Mounted TV Unit",
+    price:19999,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/565430/product/FVSGAV33WN20523_LP.jpg?1654238428",
+}
+
+,
+{
+    id:20,
+    name:"Iwaki Engineered Wood Swivel TV Unit",
+    price:14000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/697191/product/LP.jpg?1666985259",
+}
+
+,
+{
+    id:21,
+    name:"Kunsua Engineered Wood Wall Mounted TV Unit ",
+    price:5000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/565697/product/FVSGAV33WN20531_LP.jpg?1654238622",
+}
+
+,
+{
+    id:22,
+    name:"Aero Engineered Wood Wall Mounted TV Unit",
+    price:3000,
+    Category:"TV Units",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/566104/product/FVSGAV33WN20513_LP.jpg?1654238878",
+}
+
+,
+{
+    id:23,
+    name:"Xcelo Swivel Fabric Study Chair In Black Colour",
+    price:4999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/541819/slide/666x363/FVSTCH51BK37577_1.jpg?1651764647",
+}
+,
+{
+    id:24,
+    name:"Stylish Swivel Fabric Study Chair In Grey",
+    price:5999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/354358/product/Kerensa_Gaming_Chair_LP.jpg?1626756234",
+}
+,
+{
+    id:25,
+    name:"Aurelio Solid Wood Study Chair",
+    price:2999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/466968/product/Aurelio_Study_Chair_Delf_Blue_LP.jpg?1640600042",
+}
+,
+{
+    id:26,
+    name:"Doris Leatherette Accent Chair In White Colour",
+    price:8999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/219911/product/doris_swivel_accent_chair_white_replace_LP.jpg?1569471213",
+}
+,
+{
+    id:27,
+    name:"Laylah Swivel Plastic Study Chair In Grey Colour",
+    price:6999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/532931/product/FVSTCH81GY37679_LP.jpg?1651233068",
+}
+,
+{
+    id:28,
+    name:"Shirly Metal Study Chair In Black Colour",
+    price:12999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/365967/product/Shirly_Study_Chair_LP.jpg?1628171777",
+}
+,
+{
+    id:29,
+    name:"Abad Study Chair",
+    price:15999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/366336/product/Abad_Study_Chair_LP.jpg?1628238060",
+}
+,
+{
+    id:30,
+    name:"Baxlee Swivel Fabric And Plastic Study Chair ",
+    price:11999,
+    Category:"Chair",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/365105/product/Baxlee_Study_Chair_LP.jpg?1628169908",
+}
+,
+{
+    id:31,
+    name:"Twain Engineered Wood Study Table",
+    price:9999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/223893/product/Twain_Study_Table_LP.jpg?1544673336",
+}
+,
+{
+    id:32,
+    name:"Kevin Engineered Wood Study Table",
+    price:3999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/579149/product/Kevin_Study_compact_study_table_Classic_WalnutLP_1.jpg?1655450258",
+}
+
+,
+{
+    id:33,
+    name:"Graham Engineered Wood Study Table",
+    price:8999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/313271/product/Graham_Study_Table_Classic_Walnut_LP.jpg?1610514870",
+}
+
+,
+{
+    id:34,
+    name:"Juda Engineered Wood Study Table",
+    price:999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/409216/product/Zhuri_Office_Table_White_LP.jpg?1634112497",
+}
+
+,
+{
+    id:35,
+    name:"Delrick Engineered Wood Study Table ",
+    price:19999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/408869/product/Araceli_Office_Table_White_LP.jpg?1634111230",
+}
+
+,
+{
+    id:36,
+    name:"Sidney Engineered Wood Study Table",
+    price:15999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/422828/product/Sidney_Study_Table_Rustik_Oak_LP.jpg?1636954591",
+}
+
+,
+{
+    id:37,
+    name:"Niccol Engineered Wood Study Table",
+    price:25999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/70365/product/Niccol_Adjustable_Study_Table_Golden_Oak_00_LP.jpg?1452063324",
+}
+
+,
+{
+    id:38,
+    name:"Bradbury Solid Wood Study Table",
+    price:20999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/351172/product/Bradbury_Desk_Size_large_Finish_Mango_Mahogany_LP.jpg?1624945254",
+}
+
+,
+{
+    id:39,
+    name:"Malabar Solid Wood Study Table ",
+    price:16999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/339141/product/Malabar_Compact_Study_Table_Amber_Walnut_Finish_LP.jpg?1622090367",
+}
+
+,
+{
+    id:40,
+    name:"Pinto Engineered Wood Study Table ",
+    price:14999,
+    Category:"Study-table",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/312754/product/Pinto_Study_Table_LP.jpg?1608098507",
+}
+
+,
+{
+    id:41,
+    name:"Zoey Engineered Wood Queen Size Bed ",
+    price:15999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/taxon_images/taxon/16635/taxon_col_3/All_bed.jpg",
+}
+
+,
+{
+    id:42,
+    name:"Tahiti Solid Wood Queen Size Bed",
+    price:14999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/428215/product/Faroe_upholstered_stor_bed_grey_queen_LP.jpg?1637242392",
+}
+
+,
+{
+    id:43,
+    name:"Boston Solid Wood Size Bed In Teak Finish",
+    price:20999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/222566/product/Florence_Bed_LP.jpg?1543832550",
+}
+
+,
+{
+    id:44,
+    name:"Bean Solid Wood King Size Upholstered Bed In Beige Finish",
+    price:999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/632472/product/FVBDNS62BG22159_LP.JPG?1658737964",
+}
+
+,
+{
+    id:45,
+    name:"Cassiope Engineered Wood King Size Drawer Storage Upholstered Bed In Finish",
+    price:25999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/403019/product/LP.jpg?1674450191",
+}
+
+,
+{
+    id:46,
+    name:"Tahiti Solid Wood Queen Size Bed In Mahogany Finish",
+    price:30999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/161339/product/Tahiti_Platform_bed_mahogany_Replace_LP.jpg?1568958752",
+}
+
+,
+{
+    id:47,
+    name:"Watson Bed",
+    price:999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/280407/product/watson_LP_Replace_queen.jpg?1568958813",
+}
+
+,
+{
+    id:48,
+    name:"Georgia Solid Wood King Size Hydraulic Storage Bed In Walnut Finish",
+    price:20999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/414681/product/Georgia_King_Bed_With_Hydraulic_Storage_LP.jpg?1635335940",
+}
+
+,
+{
+    id:49,
+    name:"Diamond Solid Wood Queen Size Hydraulic Storage Bed In Walnut Finish",
+    price:30999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/358577/product/Georgia_Bed_With_Hydraulic_Storage_LP.jpg?1627127097",
+}
+
+,
+{
+    id:50,
+    name:"Yorktown Single Bed With Essential Foam Mattress",
+    price:12999,
+    Category:"Bed",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/240147/product/YorktownSingle_Bed_LP.jpg?1547802685",
+}
+
+,
+{
+    id:51,
+    name:"Shaela Natural Linen Shade Floor Lamp",
+    price:2999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/488297/product/VALTFL62NA12268_LP.jpg?1649155131",
+}
+
+,
+{
+    id:52,
+    name:"Faraday Floor Lamp With Side Table",
+    price:1999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/698263/product/Fareaday_Floor_lamp_LP.jpg?1667811725",
+}
+
+,
+{
+    id:53,
+    name:"Endeavour Tripod Floor Lamp",
+    price:5999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/300626/product/Endeavor_Lamp_LP.jpg?1600695077",
+}
+
+,
+{
+    id:54,
+    name:"Cardiff Table Lamp",
+    price:1999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/302840/product/Cardiff_Table_Lamp_LP.jpg?1604139070",
+}
+
+,
+{
+    id:55,
+    name:"Cumberland Table Lamp",
+    price:999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/302910/product/Cumberland_Table_Lamp_White_LP.jpg?1604158650",
+}
+
+,
+{
+    id:56,
+    name:"Defne Table Lamp",
+    price:999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/315960/product/Defne_Table_Lamp_red_LP.jpg?1613937318",
+}
+
+,
+{
+    id:57,
+    name:"Kodiak Grey Iron & Cloth Shade Table Lamp With Glass Base",
+    price:2999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/625685/product/VALTTL71OR510366_LP.JPG?1658144239",
+}
+
+,
+{
+    id:58,
+    name:"Crane Study Lamp",
+    price:5999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/476807/product/Crane_Study_Lamp__White_LP.JPG?1646890065",
+}
+
+,
+{
+    id:59,
+    name:"Brass Antique Aluminum Study Lamp",
+    price:9999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/488291/product/VALTSL86BA12255_LP.jpg?1649155128",
+}
+
+,
+{
+    id:60,
+    name:"Kobe Black Metal Shade Study Lamp With Metal Base",
+    price:6999,
+    Category:"Lights",
+    discriptons:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ratione.",
+    image:"https://www.ulcdn.net/images/products/410527/product/Tammey_-_NA_Study_Lamp_Golden_LP.jpg?1634218012",
+}
+
+
+
+
 
 ]
